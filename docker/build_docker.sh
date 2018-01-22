2344eac37d045bd2481c1562c1d7b7b0435ec4a8
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2018-01-22 14:47:51 -0500

    Added 1.9.16 version and a new minimal base image

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -2,7 +2,7 @@
 
 
 # build V2
-docker build -t nistmni/minc-toolkit:1.9.15 minc-toolkit-v2_ubuntu-16.04_x64
+docker build -t nistmni/minc-toolkit:1.9.16 minc-toolkit-v2_ubuntu-16.04_x64
 
 # build V1
 docker build -t nistmni/minc-toolkit:1.0.09 minc-toolkit-v1_ubuntu-16.04_x64
